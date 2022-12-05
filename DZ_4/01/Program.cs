7c1cc60fef655b2813dba52be915769af395a238
Ivannikov Roman <shark71@inbox.ru>
2022-12-05 20:29:06 +0300

    Решили первую задачу возведение в степень

---
+++ DZ_4/01/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+// Реализовать через функции.
+
+
+int numberA = Prompt("Введите первое число ");
+int numberB = Prompt("Введите второе число");
+Stepen(numberA, numberB);
+
+void Stepen(int a, int b)
+{
+    int result = 1;
+    for(int i = 1; i <= b; i++)
+    {
+        result = result * a;
+        
+    }
+    Console.WriteLine($"Число {numberA} в степени {numberB} = {result}");
+}
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
