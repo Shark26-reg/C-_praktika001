8ab00289bae4cc8d77ad53f9a8a403e441b75e27
Ivannikov Roman <shark71@inbox.ru>
2022-12-05 16:08:06 +0300

    Решили 2 задачу по практике

---
+++ Praktika4/02/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} >");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Counter(int number)
+{
+    int count = 0;
+    while (number > 0)
+        {
+            count ++;
+            number /=10;
+        }
+    return count;
+}
+
+int number = Prompt ("Введите число ");
+Console.WriteLine($"В чиле {number} = {Counter(number)} цифр");
+
+
