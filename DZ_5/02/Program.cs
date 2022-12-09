c7d3782c60628ac174ae826874a920d444a33a41
Ivannikov Roman <shark71@inbox.ru>
2022-12-09 13:40:48 +0300

    Решили задачачу сумма элементов стоящих на нечетных позициях

---
+++ DZ_5/02/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+
+int[] GenerateArray(int Length)
+{
+
+    int[] array = new int[Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        System.Console.Write(array[j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Search(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum = sum + array[i];
+        
+    }
+    return sum;
+}
+
+
+int Lenght = Promt("Введите длину массива");
+
+
+
+int[] array = GenerateArray(Lenght);
+PrintArray(array);
+
+
+
+
+System.Console.WriteLine($"сумма элементов, стоящих на нечётных позициях = {Search(array)}");
