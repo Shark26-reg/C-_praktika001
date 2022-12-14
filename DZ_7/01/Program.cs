119000a0361000cbc3b0643bc8f9895ca3c2a1b9
Ivannikov Roman <shark71@inbox.ru>
2022-12-14 15:35:21 +0300

    Решили задачачу двумерный массив размером mxn

---
+++ DZ_7/01/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+
+int Prompt(string message)
+{
+    Console.Write($"{message} >");              // Вывод приглашения
+    return Convert.ToInt32(Console.ReadLine()); // ввод числа
+}
+// Заполнение массива рандомными вещественными числами
+void FillCreateArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+
+}
+//  Функция вывода массива 
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int Rows = Prompt("Input number of rows >");
+int Coloums = Prompt("Input number of coloums >");
+double[,] array = new double[Rows, Coloums];
+FillCreateArray(array);
+PrintArray(array);
+
