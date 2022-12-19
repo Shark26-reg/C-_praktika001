44434862fcbcd103681ed99aaeed4a17a19ffb22
Ivannikov Roman <shark71@inbox.ru>
2022-12-19 15:17:05 +0300

    Решили задачачу которая выводит сумму натуральных чисел в промежутке

--- DZ_9/01/Program.cs
+++ DZ_9/01/Program.cs
@@ -2,24 +2,26 @@
 // M = 1; N = 5 -> "2, 4"
 // M = 4; N = 8 -> "4, 6, 8"
 
+
+void PrintNumbers(int numberStart, int numberFinish)
+{
+    if (numberStart - 1 == numberFinish)
+    {
+        return;
+    }
+    if (numberFinish % 2 == 0)
+
+    System.Console.Write($"{numberFinish};");    
+    PrintNumbers(numberStart, numberFinish - 1);    
+}
+  
 int Prompt(string message)
 {
     System.Console.Write(message);
     return int.Parse(Console.ReadLine());
 }
 
-void PrintNumbers(int number)
-{
-    if (number == 0)
-    {
-        return;
-    }
-    if (number % 2 == 0)
-    
-    System.Console.Write($"{number}; ");
-    PrintNumbers(number - 1);       
-}
-
-int number = Prompt("Введите число ");
-PrintNumbers(number);
+int numberStart = Prompt("Введите число (начало промежутка) ");
+int numberFinish = Prompt("Введите число (конец промежутка) ");
+PrintNumbers(numberStart, numberFinish);
 
