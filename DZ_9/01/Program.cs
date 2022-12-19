15689cc0b7958282553d6def50263d1e1f95068d
Ivannikov Roman <shark71@inbox.ru>
2022-12-19 15:07:39 +0300

    Решили задачачу которая выводит все четные натуральные числа в заданном  промежутке

---
+++ DZ_9/01/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintNumbers(int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    if (number % 2 == 0)
+    
+    System.Console.Write($"{number}; ");
+    PrintNumbers(number - 1);       
+}
+
+int number = Prompt("Введите число ");
+PrintNumbers(number);
+
